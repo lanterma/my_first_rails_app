3f97931a1d49de344d04da1eedf273de27f9b3e9
Matt Lanterman <Lanterma@gmail.com>
2015-03-17 20:24:14 -0600

    Initial commit on My First Rails App
    *Working Time/now
    *Say Hi to @name1 and @name2
    
    [#90137022]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
   Rails.application.routes.draw do
-get 'hello/:name' => 'welcome#hello'
+  get 'hello' => 'welcome#hello'
+  get 'hello/:name' => 'welcome#hello'
+
+  get 'time/now' => 'time#now'
+
+  get 'introduce/:name1/and/:name2' => 'introduce#names'
 
 end
